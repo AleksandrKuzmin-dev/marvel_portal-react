ac4fecb1695c5356003fd10d6585fcaf927ca1c9
Aleksandr Kuzmin <a-kuzmin.dev@yandex.ru>
2024-03-07 18:22:03 +0400

    Добавил динамические иморты (lazy)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,28 +1,34 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { Suspense, lazy} from "react";
 
 import AppHeader from "../appHeader/AppHeader";
 import MainPage from "../pages/MainPage";
 import ComicsPage from "../pages/ComicsPage";
-import SingleComicPage from "../pages/SingleComicPage";
-import Page404 from "../pages/Page404";
+import Spinner from "../spinner/Spinner.js";
+
+const Page404 = lazy(() => import('../pages/Page404.js'));
+const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
+
 
 const App = () => {
 
     return (
+        <Suspense fallback={<Spinner />}>
         <Router>
             <div className="app">
                     <Routes>
                         <Route path='/' element={<AppHeader/>}>
-                            <Route index element={<MainPage/>} />
+                            <Route index element={<MainPage />} />
                             <Route path='/comics'>
-                                <Route index element={<ComicsPage/>}/>
-                                <Route path=':comicId' element={<SingleComicPage/>} />
+                                <Route index element={<ComicsPage />} />
+                                <Route path=':comicId' element={<SingleComicPage />} />
                             </Route>
-                            <Route path='*' element={<Page404/>} />
+                            <Route path='*' element={<Page404 />} />
                         </Route>
                     </Routes>
             </div>
         </Router>
+        </Suspense>
     )
 }
 
